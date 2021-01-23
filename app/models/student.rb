30a47daae3bd858287d7b5fb1ae5b882ed1ef389
Wesley <wes.beck21@outlook.com>
2021-01-22 19:57:09 -0800

    commit tree ac749eabe7b3b7cda994359254823c4668fecde0

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  validates :name, :hometown, :birthday, presence: true
+
+  def self.search(query)
+    if query.present?
+      self.where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
